e8e848e59433875a7463761946724967cac1c221
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-25 19:23:48 +0500

    Optimize scripts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// elements
 const [paper, scissors, rock] = document.querySelectorAll("[data-pick]"),
   elScorePlayer = document.querySelector("[data-player-score]"),
   elScoreComputer = document.querySelector("[data-computer-score]"),
@@ -5,78 +6,49 @@ const [paper, scissors, rock] = document.querySelectorAll("[data-pick]"),
   elComputerResult = document.querySelector("[data-computer-result]"),
   elResultText = document.querySelector("[data-result-text]");
 
+// variables
 let scorePlayer = 0,
   scoreComputer = 0;
 
-if (window.innerWidth <= 576) {
-  elScoreComputer.previousElementSibling.textContent = "Phone Score";
-} else {
-  elScoreComputer.previousElementSibling.textContent = "Computer Score";
-}
+if (window.innerWidth <= 576) elScoreComputer.previousElementSibling.textContent = "Phone Score";
+else elScoreComputer.previousElementSibling.textContent = "Computer Score";
 
-document.addEventListener("click", (evt) => {
-  clickPlayerChoice(evt);
-  restartBtnClick(evt);
-  resetBtnClick(evt);
-});
+// getPlayerChoice
+const getPlayerChoice = el => {
+  if (el === paper) return "paper";
+  else if (el === scissors) return "scissors";
+  else return "rock";
+};
 
-function clickPlayerChoice(evt) {
-  const el = evt.target.closest("[data-pick]");
-
-  if (!el) return;
-
-  const playerChoice = getPlayerChoice(el);
-  const computerChoice = getComputerChoice();
-  const winner = getWinner(playerChoice, computerChoice);
-  showWinner(winner, playerChoice, computerChoice);
-}
-
-function getComputerChoice() {
+// getComputerChoise
+const getComputerChoice = () => {
   const randomNum = Math.trunc(1 + Math.random() * 3);
+
   if (randomNum === 1) return "paper";
   else if (randomNum === 2) return "scissors";
   else return "rock";
-}
+};
 
-function getPlayerChoice(el) {
-  if (el === paper) {
-    return "paper";
-  } else if (el === scissors) {
-    return "scissors";
-  } else if (el === rock) {
-    return "rock";
-  } else return;
-}
+// getWinner
+const getWinner = (player, computer) => {
+  if (player === computer) return "draw";
 
-function getWinner(player, computer) {
-  if (player === computer && computer !== "") {
-    return "draw";
-  } else if (player === "paper") {
-    if (computer === "rock") {
-      return "player";
-    } else {
-      return "computer";
-    }
+  if (player === "paper") {
+    if (computer === "rock") return "player";
+    else return "computer";
   } else if (player === "scissors") {
-    if (computer === "paper") {
-      return "player";
-    } else {
-      return "computer";
-    }
+    if (computer === "paper") return "player";
+    else return "computer";
   } else if (player === "rock") {
-    if (computer === "paper") {
-      return "computer";
-    } else {
-      return "player";
-    }
+    if (computer === "paper") return "computer";
+    else return "player";
   }
-}
+};
 
-function showWinner(winner, playerChoice, computerChoice) {
+// showWinner
+const showWinner = (winner, playerChoice, computerChoice) => {
   elComputerResult.parentElement.parentElement.classList.remove("hidden");
-  elComputerResult.parentElement.parentElement.previousElementSibling.classList.add(
-    "hidden"
-  );
+  elComputerResult.parentElement.parentElement.previousElementSibling.classList.add("hidden");
 
   elPlayerResult.src = `images/icon-${playerChoice}.svg`;
   elComputerResult.src = `images/icon-${computerChoice}.svg`;
@@ -91,33 +63,34 @@ function showWinner(winner, playerChoice, computerChoice) {
     scoreComputer++;
     elScoreComputer.textContent = scoreComputer;
     elResultText.textContent = `YOU LOSE`;
-  } else if (winner === "draw") {
-    elResultText.textContent = `DRAW`;
-  }
+  } else if (winner === "draw") elResultText.textContent = `DRAW`;
 
-  if (scoreComputer > 0 || scorePlayer > 0) {
-    console.log(scoreComputer);
-    console.log(scorePlayer);
-    elComputerResult.parentElement.parentElement.nextElementSibling.classList.remove(
-      "hidden"
-    );
-  }
-}
+  if (scoreComputer > 0 || scorePlayer > 0) elComputerResult.parentElement.parentElement.nextElementSibling.classList.remove("hidden");
+};
 
-function restartBtnClick(evt) {
+// clickPlayerChoice
+const clickPlayerChoice = evt => {
+  const el = evt.target.closest("[data-pick]");
+  if (!el) return;
+
+  const playerChoice = getPlayerChoice(el);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, playerChoice, computerChoice);
+};
+
+// restartBtnClick
+const restartBtnClick = evt => {
   const el = evt.target.closest("[data-restart-btn]");
-
   if (!el) return;
 
   el.parentElement.parentElement.classList.add("hidden");
-  el.parentElement.parentElement.previousElementSibling.classList.remove(
-    "hidden"
-  );
-}
+  el.parentElement.parentElement.previousElementSibling.classList.remove("hidden");
+};
 
-function resetBtnClick(evt) {
+// resetBtnClick
+const resetBtnClick = evt => {
   const el = evt.target.closest("[data-reset-btn]");
-
   if (!el) return;
 
   scorePlayer = 0;
@@ -125,8 +98,13 @@ function resetBtnClick(evt) {
   elScorePlayer.textContent = scorePlayer;
   elScoreComputer.textContent = scoreComputer;
   elPlayerResult.parentElement.parentElement.classList.add("hidden");
-  elPlayerResult.parentElement.parentElement.previousElementSibling.classList.remove(
-    "hidden"
-  );
+  elPlayerResult.parentElement.parentElement.previousElementSibling.classList.remove("hidden");
   el.classList.add("hidden");
-}
+};
+
+// click event
+document.addEventListener("click", evt => {
+  clickPlayerChoice(evt);
+  restartBtnClick(evt);
+  resetBtnClick(evt);
+});
